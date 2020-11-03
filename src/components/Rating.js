43612178f49d33e05b15b397bdf03442a8247931
Devfarley <66633806+Devfarley@users.noreply.github.com>
2020-11-02 20:39:24 -0700

    Created individual product pages

--- src/components/Rating.js
+++ src/components/Rating.js
@@ -8,7 +8,7 @@ const Rating = ({ value, text, color }) => {
                 <i style={{color}} className={value >= 1 ? 'fas fa-star': value >= 0.5 ? 'fas fa-star-half-alt': 'far fa-star'}></i>
             </span>
             <span>
-                <i style={{color}} className={value >= 2 ? 'fas fa-star': value >= 1.5 ? 'fas fa-star-half-alt': 'far fa-star'}></i>
+                <i style={{color}}  className={value >= 2 ? 'fas fa-star': value >= 1.5 ? 'fas fa-star-half-alt': 'far fa-star'}></i>
             </span>
             <span>
                 <i style={{color}} className={value >= 3 ? 'fas fa-star': value >= 2.5 ? 'fas fa-star-half-alt': 'far fa-star'}></i>
